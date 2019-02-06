428ea9c8fb4b694d314dfb8d1c67ba28606a9043
0xKoddoelo <0xKoddoelo@gmail.com>
2019-02-07 02:59:31 +0700

    Change Structure

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ if (navigator.serviceWorker) {
   console.log('Service Worker Supported');
   window.addEventListener('load', () => {
     console.log('event listener load');
-    navigator.serviceWorker.register('../sw_entire_auto.js').then(r => {
+    navigator.serviceWorker.register('./sw_entire_auto.js').then(r => {
       console.log('Service Worker: Registered');
     }).catch(err => console.log('Service Worker: ERROR', err));
   })
